d876ad611317d53cb02b32e978d3875332bee179
Spiral11 <35121523+Spiral11@users.noreply.github.com>
2018-07-24 10:55:10 -0400

    Create Cmd Shortcut Script

---
+++ shortcuts.sh
@@ -0,0 +1,29 @@
+#Add shortcuts to common commands, run as your main user
+
+printf "zc() {
+  %s \"\$@\"
+}
+export -f zc\n" "sudo docker exec zen-node gosu user zen-cli " >> ~/.bashrc
+
+printf "zen-cli() {
+  %s \"\$@\"
+}
+export -f zen-cli\n" "sudo docker exec zen-node gosu user zen-cli " >> ~/.bashrc
+
+printf "sc() {
+  %s \"\$@\"
+}
+export -f sc\n" "sudo systemctl " >> ~/.bashrc
+
+printf "lt() {
+  %s \"\$@\"
+}
+export -f lt\n" "sudo docker logs zen-secnodetracker" >> ~/.bashrc
+
+printf "lz() {
+  %s \"\$@\"
+}
+export -f lz\n" "sudo docker logs zen-node" >> ~/.bashrc
+
+source ~/.bashrc
+echo "source ~/.bashrc"
