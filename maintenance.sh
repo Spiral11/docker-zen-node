c5ca0cfb1447c21d9b3ba817bdf12b33f76ff7f4
Spiral11 <35121523+Spiral11@users.noreply.github.com>
2018-07-24 12:55:00 -0400

    Create maintenance.sh

---
+++ maintenance.sh
@@ -0,0 +1,8 @@
+#Update apps / system
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+sudo apt-get clean
+
+$Clean docker images to free disk space
+sudo docker system prune -a
