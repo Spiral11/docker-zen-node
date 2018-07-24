949fc7c9e94f3d0786b22a6c6bc582af849d54f1
Spiral11 <35121523+Spiral11@users.noreply.github.com>
2018-07-24 13:16:01 -0400

    Update shortcuts.sh

--- shortcuts.sh
+++ shortcuts.sh
@@ -5,11 +5,6 @@ source ~/.bashrc
 
 # Add our new shortcut commands
 
-printf "zc() {
-  %s \"\$@\"
-}
-export -f zc\n" "sudo docker exec zen-node gosu user zen-cli " >> ~/.bashrc
-
 printf "zen-cli() {
   %s \"\$@\"
 }
@@ -25,17 +20,6 @@ printf "logs() {
 }
 export -f logs\n" "sudo docker logs " >> ~/.bashrc
 
-printf "lt() {
-  %s \"\$@\"
-}
-export -f lt\n" "sudo docker logs zen-secnodetracker" >> ~/.bashrc
-
-printf "lz() {
-  %s \"\$@\"
-}
-export -f lz\n" "sudo docker logs zen-node" >> ~/.bashrc
-
-sleep 3
-
 source ~/.bashrc
-echo "source ~/.bashrc"
+
+echo "Shortcuts Added"
