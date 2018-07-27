e37875dc8ba8fcc3cc0f8fb3a8eb1e56c9120910
Spiral11 <35121523+Spiral11@users.noreply.github.com>
2018-07-26 20:23:15 -0400

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Clean docker images to free disk space
+sudo docker system prune -a
+
+unset UCF_FORCE_CONFFOLD
+export UCF_FORCE_CONFFNEW=YES
+sudo ucf --purge /boot/grub/menu.lst
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+
 #Update apps / system
-sudo apt-get -y update
-sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 sudo apt-get clean
-
-#Clean docker images to free disk space
-sudo docker system prune -a
